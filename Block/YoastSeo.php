738c682228949c99e3b4cbc5b1a688e191c8643f
Daviot <daviot@live.at>
2024-06-07 15:03:06 +0200

    refactor: make code backwards compatible

--- Block/YoastSeo.php
+++ Block/YoastSeo.php
@@ -59,15 +59,19 @@ class YoastSeo extends Template
             ScopeInterface::SCOPE_STORES
         );
 
-        if ($canonical
+        if (
+            $canonical
             && $parameterStrip == true
-            && strstr(substr($canonical, strrpos($canonical, '/')), '?') !== false) {
+            && strstr(substr($canonical, strrpos($canonical, '/')), '?') !== false
+        ) {
             $canonical = substr($canonical, 0, strrpos($canonical, '/'));
         }
 
-        if ($canonical
+        if (
+            $canonical
             && substr($canonical, -5) !== '.html'
-            && substr($canonical, -1) !== '/') {
+            && substr($canonical, -1) !== '/'
+        ) {
             $canonical .= '/';
         }
 
@@ -134,18 +138,11 @@ class YoastSeo extends Template
      */
     public function getTwitterSite()
     {
-        $twitterSite = $this->_scopeConfig->getValue(
+        return $this->getTwitterValue($this->_scopeConfig->getValue(
             'yoastseo/twitter/company_account',
             ScopeInterface::SCOPE_STORES
-        ) ?? "";
-        if (substr($twitterSite, 0, 1) !== '@') {
-            $twitterSite = '@' . $twitterSite;
-        }
-        if ($twitterSite === '@') {
-            $twitterSite = '';
-        }
-
-        return $twitterSite;
+        )
+        );
     }
 
     /**
@@ -153,17 +150,25 @@ class YoastSeo extends Template
      */
     public function getTwitterCreator()
     {
-        $twitterUser = $this->_scopeConfig->getValue(
+        return $this->getTwitterValue($this->_scopeConfig->getValue(
             'yoastseo/twitter/manager_account',
             ScopeInterface::SCOPE_STORE
-        ) ?? "";
-        if (substr($twitterUser, 0, 1) !== '@') {
-            $twitterUser = '@' . $twitterUser;
+        )
+        );
+    }
+
+    private function getTwitterValue($value)
+    {
+        if (\is_null($value)) {
+            $value = "";
         }
-        if ($twitterUser === '@') {
-            $twitterUser = '';
+        if (\substr($value, 0, 1) !== '@') {
+            $value = '@' . $value;
+        }
+        if ($value === '@') {
+            $value = '';
         }
 
-        return $twitterUser;
+        return $value;
     }
 }
